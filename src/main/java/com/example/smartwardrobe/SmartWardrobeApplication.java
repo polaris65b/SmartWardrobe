94868248320951dcbfe0b8ece31e99714fe5e213
polaris65b <polaris65b@gmail.com>
2025-05-12 23:52:06 +0900

    Initial commit

---
+++ src/main/java/com/example/smartwardrobe/SmartWardrobeApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartwardrobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartWardrobeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartWardrobeApplication.class, args);
+    }
+
+}
