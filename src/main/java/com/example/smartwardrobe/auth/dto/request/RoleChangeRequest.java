f28b03e41f6b2ded35190585cf907aa736093793
polaris65b <polaris65b@gmail.com>
2025-05-14 23:14:43 +0900

    refactor : 테스트 코드를 위한 애노테이션 추가

--- src/main/java/com/example/smartwardrobe/auth/dto/request/RoleChangeRequest.java
+++ src/main/java/com/example/smartwardrobe/auth/dto/request/RoleChangeRequest.java
@@ -1,11 +1,13 @@
 package com.example.smartwardrobe.auth.dto.request;
 
 import com.example.smartwardrobe.user.entity.UserRole;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class RoleChangeRequest {
     private UserRole role;
 }
